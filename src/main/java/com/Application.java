8fb9a3baf84aa3f29c9da252fbe74b54f536c7ba
wangmengwei <1776408028@qq.com>
2019-08-29 09:17:10 +0800

    first commit

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: demo
+ * @description: 启动类
+ * @author: MengW9
+ * @create: 2019-08-28
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
